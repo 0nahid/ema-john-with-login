e35ccad88180c94133e6c43a0debdcf6fb7996cd
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-11 17:49:24 +0600

    added react hook form

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form";
+import './Shipping.css'
+import useAuth from './../../hooks/useAuth';
 export default function Shipping() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { user } = useAuth();
+    const onSubmit = data => {
+        console.log(data)
+    }
+
     return (
-        <div>
-            <h1>This is shipping</h1>
+        <div className="shipping-form">
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />
+                    <input placeholder="Address" defaultValue="" {...register("address")} />
+                    <input placeholder="Phone" defaultValue="" {...register("phone")} />
+                    <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
+                    {errors.email && <span className="error">This field is required</span>}
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     )
 }
